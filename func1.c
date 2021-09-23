bb97bf7f617f8a8b11f0bda4dda5303a723b80d3
edocindnn <89440905+edocindnn@users.noreply.github.com>
2021-09-23 14:46:46 +0530

    Update func1.c

--- func1.c
+++ func1.c
@@ -4,6 +4,11 @@ int main()
     int n,sum=0,m;
     printf("Enter a number:");
     scanf("%d",&n);
+    sumd(n, sum, m);
+    return 0;
+}
+int sumd(int n, int sum, int m)
+{
     while(n>0)
     {
         m=n%10;
@@ -11,5 +16,5 @@ int main()
         n=n/10;
     }
     printf("Sum is=%d",sum);
-    return 0;
 }
+
