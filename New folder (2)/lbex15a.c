05a60e674230d8e854c08b75bd4c2ff70692ea52
edocindnn <89440905+edocindnn@users.noreply.github.com>
2021-09-26 22:59:29 +0530

    Add files via upload

---
+++ New folder (2)/lbex15a.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+void main(int argc,char *argv[])
+{
+    char ch;
+    FILE *mainfile;
+    FILE *copyfile;
+    //argc hold this C file + main file + copy file = 3
+    if(argc != 3){
+        printf("\nCan not open file for reading…");
+        exit(1);
+    }
+    mainfile = fopen(argv[1],"r");
+    copyfile = fopen(argv[2],"w");
+    if(mainfile==NULL || copyfile==NULL){
+        printf("File opening Error or file not Found");
+        exit(1);
+    }
+    ch=fgetc(mainfile);
+    while ((ch!=EOF))
+    {
+        fputc(ch,copyfile);
+        ch = fgetc(mainfile);
+    }
+    printf("\nFile copied successful\n");
+    fclose(mainfile);
+    fclose(copyfile);
+
+    getch();
+}
